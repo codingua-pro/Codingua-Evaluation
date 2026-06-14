bf4e6170996bfc05ed9d5e3a1c11e31043a77513
Codingua <159129943+codingua-pro@users.noreply.github.com>
2026-06-14 16:29:53 +0300

    update

--- app.js
+++ app.js
@@ -11,7 +11,7 @@
 //  ⚠️  Replace these values with your actual Firebase project config
 // ══════════════════════════════════════════════════════════════
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
